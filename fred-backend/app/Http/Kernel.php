4c76b0f94b9dc1c8d8370dc67d546e401a105194
Jonas Looser <1stkarkan@gmail.com>
2019-06-11 13:49:51 +0200

    fix for last commit

--- fred-backend/app/Http/Kernel.php
+++ fred-backend/app/Http/Kernel.php
@@ -18,8 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\Cors::class,
+        \App\Http\Middleware\TrustProxies::class
     ];
 
     /**
@@ -63,6 +62,7 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'app-user' =>  \App\Http\Middleware\CheckUserHash::class,
         'api-key' =>  \App\Http\Middleware\CheckApiKey::class,
+        'cors' => \App\Http\Middleware\Cors::class,
     ];
 
     /**
